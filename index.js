f367272e552fea8ef279b7a0822b9d00e7543558
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 16:06:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-let totalBatteries = 31;
+let sum = 0;
+for(const number of batteryBatches){
+  sum+=number;
+}
+let totalBatteries = sum;
